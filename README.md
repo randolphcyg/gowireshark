36d00d96fe2895e6f164a8bf7ce214120411ae87
蔡迎港 <cyg0054@gmail.com>
2022-09-05 18:05:49 +0800

    [INIT]upload code without ddl files

---
+++ README.md
@@ -0,0 +1,92 @@
+# gowireshark
+
+> gowireshark是一个提供wireshark协议解析功能的golang包
+
+- 暂仅支持linux平台，此库在ubuntu22.04中开发测试
+- 基于 wireshark3.6.7
+- 用c和go封装wireshark，是一个golang包
+---
+## 1.项目结构说明
+
+1. 依赖组件：
+   - lwiretap、lwsutil、lwireshark
+   - glib-2.0
+   - github.com/pkg/errors
+   - github.com/sirupsen/logrus
+
+2. 项目结构
+
+```shell
+gowireshark
+├── README.md
+├── common/                       // 项目依赖的基础包
+│   ├── middleware/               // 中间件 日志logrus
+│   └── tool/                     // 工具类
+├── go.mod
+├── go.sum
+├── gowireshark.go
+├── include/
+│   ├── lib.h                     // 经过封装暴露出的wireshark接口
+│   └── wireshark/                // wireshark源码
+├── lib.c
+├── libs/                         //  基于 wireshark3.6.7 源码编译的 libwireshark.so 等动态链接库文件
+│   ├── libwireshark.so
+│   ├── libwireshark.so.15
+│   ├── libwireshark.so.15.0.7
+│   ├── libwiretap.so
+│   ├── libwiretap.so.12
+│   ├── libwiretap.so.12.0.7
+│   ├── libwsutil.so
+│   ├── libwsutil.so.13
+│   └── libwsutil.so.13.1.0
+├── pcaps/                         // pcap测试文件
+│   └── s7comm_clean.pcap
+└── tests/                         // 测试文件夹
+    └── gowireshark_test.go
+```
+
+## 2. 项目思路
+
+1. 根据wireshark源码编译libwireshark.so等动态链接库
+2. 用c封装功能并用go封装对外接口
+
+## 3. How To Use
+
+1. 环境要求：  x86-64, 安装了glib2.0
+
+2. 安装：
+
+```shell
+go get github.com/randolphcyg/gowireshark
+```
+
+3. 测试代码：
+
+```go
+package main
+
+import (
+	"fmt"
+
+	"github.com/randolphcyg/gowireshark"
+)
+
+func main() {
+	fmt.Println("测试数据包文件第一个包解析")
+	filepath := "pcaps/s7comm_clean.pcap"
+	err := gowireshark.DissectFirstPkt(filepath)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+```
+
+## 4. TODO
+
+1. 修复解析数据包过程错误问题
+2. 增加修正为输出解析后的json数据
+3. 增加实时坚挺解析功能
+
+
+
+
---
+++ gowireshark.go
@@ -0,0 +1,94 @@
+package gowireshark
+
+/*
+#cgo pkg-config: glib-2.0
+#cgo LDFLAGS: -L${SRCDIR}/libs -lwiretap -lwsutil -lwireshark
+#cgo LDFLAGS: -Wl,-rpath,${SRCDIR}/libs
+#cgo CFLAGS: -I${SRCDIR}/include/wireshark
+#cgo CFLAGS: -std=c99
+
+#include <stdio.h>
+
+#include <epan/tvbuff.h>
+#include <epan/epan.h>
+#include <cfile.h>
+#include <wiretap/wtap.h>
+#include <wsutil/privileges.h>
+#include <wiretap/wtap-int.h>
+#include <epan/column.h>
+#include <wsutil/nstime.h>
+#include <epan/frame_data_sequence.h>
+#include <epan/frame_data.h>
+#include <epan/epan_dissect.h>
+#include <epan/print.h>
+#include <epan/print_stream.h>
+
+
+int init(char *filename);
+
+void print_all_packet_text();
+
+void print_first_packet_text();
+*/
+import "C"
+import (
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/randolphcyg/gowireshark/common/middleware/logger"
+	"github.com/randolphcyg/gowireshark/common/tool"
+)
+
+var (
+	ErrFileNotFound = errors.New("cannot open file, no such file")
+)
+
+func init() {
+	// init logger
+	logger.Init()
+}
+
+// EpanVersion get epan module's version
+func EpanVersion() string {
+	return C.GoString((*C.char)(C.epan_get_version()))
+}
+
+// EpanPluginsSupported
+/** Returns_
+ *     0 if plugins can be loaded for all of libwireshark (tap, dissector, epan).
+ *     1 if plugins are not supported by the platform.
+ *    -1 if plugins were disabled in the build configuration.
+ */
+func EpanPluginsSupported() int {
+	return int(C.epan_plugins_supported())
+}
+
+// DissectFirstPkt dissect all package in a pcap file but only print the first package
+func DissectFirstPkt(filepath string) (err error) {
+	if !tool.IsFileExist(filepath) {
+		err = errors.Wrap(ErrFileNotFound, filepath)
+		log.Error(err)
+		return err
+	}
+
+	C.init(C.CString(filepath))
+
+	C.print_first_packet_text()
+
+	return
+}
+
+// DissectAllPkt dissect and print all package in a pcap file
+func DissectAllPkt(filepath string) (err error) {
+	if !tool.IsFileExist(filepath) {
+		err = errors.Wrap(ErrFileNotFound, filepath)
+		log.Error(err)
+		return err
+	}
+
+	C.init(C.CString(filepath))
+
+	C.print_all_packet_text()
+
+	return
+}
