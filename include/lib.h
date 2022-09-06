b24a19c1eb578ce62fbfec3e3211d664f906379f
蔡迎港 <cyg0054@gmail.com>
2022-09-06 15:39:34 +0800

    [FIX]format c code & fix readme

--- include/lib.h
+++ include/lib.h
@@ -1,24 +1,22 @@
 #include <stdio.h>
-
-#include <epan/tvbuff.h>
-#include <epan/epan.h>
 #include <cfile.h>
-#include <wiretap/wtap.h>
-#include <wsutil/privileges.h>
-#include <wiretap/wtap-int.h>
 #include <epan/column.h>
-#include <wsutil/nstime.h>
+#include <epan/epan.h>
+#include <epan/tvbuff.h>
 #include <epan/frame_data_sequence.h>
 #include <epan/frame_data.h>
 #include <epan/epan_dissect.h>
 #include <epan/print.h>
 #include <epan/print_stream.h>
-
-//global variable
+#include <wiretap/wtap.h>
+#include <wiretap/wtap-int.h>
+#include <wsutil/nstime.h>
+#include <wsutil/privileges.h>
+// global variable
 capture_file cfile;
-
+// init modules & init capture_file
 int init(char *filename);
-
+// print the whole frame
 void print_all_packet_text();
-
+// print the first frame
 void print_first_packet_text();
