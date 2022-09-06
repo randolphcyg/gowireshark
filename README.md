76b482c04ad1a99e5a3eeb24d42cff194690afef
蔡迎港 <cyg0054@gmail.com>
2022-09-06 15:39:34 +0800

    [FIX]format c code & fix readme

--- README.md
+++ README.md
@@ -16,34 +16,34 @@
 
 2. 项目结构
 
-```shell
-gowireshark
-├── README.md
-├── common/                       // 项目依赖的基础包
-│   ├── middleware/               // 中间件 日志logrus
-│   └── tool/                     // 工具类
-├── go.mod
-├── go.sum
-├── gowireshark.go
-├── include/
-│   ├── lib.h                     // 经过封装暴露出的wireshark接口
-│   └── wireshark/                // wireshark源码
-├── lib.c
-├── libs/                         //  基于 wireshark3.6.7 源码编译的 libwireshark.so 等动态链接库文件
-│   ├── libwireshark.so
-│   ├── libwireshark.so.15
-│   ├── libwireshark.so.15.0.7
-│   ├── libwiretap.so
-│   ├── libwiretap.so.12
-│   ├── libwiretap.so.12.0.7
-│   ├── libwsutil.so
-│   ├── libwsutil.so.13
-│   └── libwsutil.so.13.1.0
-├── pcaps/                         // pcap测试文件
-│   └── s7comm_clean.pcap
-└── tests/                         // 测试文件夹
-    └── gowireshark_test.go
-```
+   ```shell
+   gowireshark
+   ├── README.md
+   ├── common/                       // 项目依赖的基础包
+   │   ├── middleware/               // 中间件 日志logrus
+   │   └── tool/                     // 工具类
+   ├── go.mod
+   ├── go.sum
+   ├── gowireshark.go
+   ├── include/
+   │   ├── lib.h                     // 经过封装暴露出的wireshark接口
+   │   └── wireshark/                // wireshark源码
+   ├── lib.c                         // 封装的流程
+   ├── libs/                         // 基于 wireshark3.6.7 源码编译的 libwireshark.so 等动态链接库文件
+   │   ├── libwireshark.so
+   │   ├── libwireshark.so.15
+   │   ├── libwireshark.so.15.0.7
+   │   ├── libwiretap.so
+   │   ├── libwiretap.so.12
+   │   ├── libwiretap.so.12.0.7
+   │   ├── libwsutil.so
+   │   ├── libwsutil.so.13
+   │   └── libwsutil.so.13.1.0
+   ├── pcaps/                         // pcap测试文件
+   │   └── s7comm_clean.pcap
+   └── tests/                         // 测试文件夹
+       └── gowireshark_test.go
+   ```
 
 ## 2. 项目思路
 
@@ -54,38 +54,38 @@ gowireshark
 
 1. 环境要求：  x86-64, 安装了glib2.0
 
-2. 安装：
+2. 安装
 
-```shell
-go get github.com/randolphcyg/gowireshark
-```
+   ```shell
+   go get github.com/randolphcyg/gowireshark
+   ```
 
 3. 测试代码：
 
-```go
-package main
-
-import (
-	"fmt"
-
-	"github.com/randolphcyg/gowireshark"
-)
-
-func main() {
-	fmt.Println("测试数据包文件第一个包解析")
-	filepath := "pcaps/s7comm_clean.pcap"
-	err := gowireshark.DissectFirstPkt(filepath)
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-```
+   ```go
+   package main
+   
+   import (
+       "fmt"
+   
+       "github.com/randolphcyg/gowireshark"
+   )
+   
+   func main() {
+       fmt.Println("测试数据包文件第一个包解析")
+       filepath := "pcaps/s7comm_clean.pcap"
+       err := gowireshark.DissectFirstPkt(filepath)
+       if err != nil {
+           fmt.Println(err)
+       }
+   }
+   ```
 
 ## 4. TODO
 
 1. 修复解析数据包过程错误问题
 2. 增加修正为输出解析后的json数据
-3. 增加实时坚挺解析功能
+3. 增加实时监听解析功能
 
 
 
--- lib.c
+++ lib.c
@@ -1,130 +1,100 @@
 #include <include/lib.h>
-
-void cap_file_init(capture_file *cf)
-{
+void cap_file_init(capture_file *cf) {
 	/* Initialize the capture file struct */
 	memset(cf, 0, sizeof(capture_file));
 }
-
-static const nstime_t * tshark_get_frame_ts(struct packet_provider_data *prov, guint32 frame_num)
-{
-  if (prov->ref && prov->ref->num == frame_num)
-    return &prov->ref->abs_ts;
-
-  if (prov->prev_dis && prov->prev_dis->num == frame_num)
-    return &prov->prev_dis->abs_ts;
-
-  if (prov->prev_cap && prov->prev_cap->num == frame_num)
-    return &prov->prev_cap->abs_ts;
-
-  if (prov->frames) {
-     frame_data *fd = frame_data_sequence_find(prov->frames, frame_num);
-
-     return (fd) ? &fd->abs_ts : NULL;
-  }
-
-  return NULL;
+static const nstime_t * tshark_get_frame_ts(struct packet_provider_data *prov, guint32 frame_num) {
+	if (prov->ref && prov->ref->num == frame_num)
+		    return &prov->ref->abs_ts;
+	if (prov->prev_dis && prov->prev_dis->num == frame_num)
+		    return &prov->prev_dis->abs_ts;
+	if (prov->prev_cap && prov->prev_cap->num == frame_num)
+		    return &prov->prev_cap->abs_ts;
+	if (prov->frames) {
+		frame_data *fd = frame_data_sequence_find(prov->frames, frame_num);
+		return (fd) ? &fd->abs_ts : NULL;
+	}
+	return NULL;
 }
-
-void clean()
-{
+void clean() {
 	if (cfile.provider.frames != NULL) {
-        /*
+		/*
          * Free a frame_data_sequence and all the frame_data structures in it.
          */
 		free_frame_data_sequence(cfile.provider.frames);
 		cfile.provider.frames = NULL;
 	}
-
 	if (cfile.provider.wth != NULL) {
-	    /** Closes any open file handles and frees the memory associated with wth. */
+		/** Closes any open file handles and frees the memory associated with wth. */
 		wtap_close(cfile.provider.wth);
 		cfile.provider.wth = NULL;
 	}
-
 	if (cfile.epan != NULL)
-		epan_free(cfile.epan);
-
-    /** cleanup the whole epan module, this is used to be called only once in a program */
+				epan_free(cfile.epan);
+	/** cleanup the whole epan module, this is used to be called only once in a program */
 	epan_cleanup();
 }
-
 /* Fill data to the capture file struct */
-int init(char *filename)
-{
-    int          err = 0;
-    gchar       *err_info = NULL;
-    e_prefs     *prefs_p;
-
-    /**
+int init(char *filename) {
+	int          err = 0;
+	gchar       *err_info = NULL;
+	e_prefs     *prefs_p;
+	/**
      * Called when the program starts, to enable security features and save
      * whatever credential information we'll need later.
      */
-    init_process_policies();
-
-    /**
+	init_process_policies();
+	/**
      * Permanently relinquish special privileges. get_credential_info()
      * MUST be called before calling this.
      */
-    relinquish_special_privs_perm();
-
-    /**
+	relinquish_special_privs_perm();
+	/**
      * @brief Initialize the Wiretap library.
      *
      * @param load_wiretap_plugins Load Wiretap plugins when initializing library.
     */
-    wtap_init(TRUE);
-
-    /**
+	wtap_init(TRUE);
+	/**
      * Init the whole epan module.
      *
      * Must be called only once in a program.
      *
      * Returns TRUE on success, FALSE on failure.
      */
-    epan_init(NULL, NULL, 0);
-
-    cap_file_init(&cfile);
-    cfile.filename = filename;
-
-    cfile.provider.wth = wtap_open_offline(cfile.filename, WTAP_TYPE_AUTO, &err, &err_info, TRUE);
-    if (cfile.provider.wth == NULL)
-    	goto fail;
-
-    cfile.count = 0;
-
-    cfile.provider.frames = new_frame_data_sequence();
-
-    static const struct packet_provider_funcs funcs = {
-        tshark_get_frame_ts,
-        NULL,
-        NULL,
-        NULL,
-      };
-    cfile.epan = epan_new(&cfile.provider, &funcs);
-
-    prefs_p = epan_load_settings();
-    build_column_format_array(&cfile.cinfo, prefs_p->num_cols, TRUE);
-
-    return 0;
-
-fail:
-	clean();
+	epan_init(NULL, NULL, 0);
+	cap_file_init(&cfile);
+	cfile.filename = filename;
+	cfile.provider.wth = wtap_open_offline(cfile.filename, WTAP_TYPE_AUTO, &err, &err_info, TRUE);
+	if (cfile.provider.wth == NULL)
+		    	goto fail;
+	cfile.count = 0;
+	cfile.provider.frames = new_frame_data_sequence();
+	static const struct packet_provider_funcs funcs = {
+		tshark_get_frame_ts,
+			        NULL,
+			        NULL,
+			        NULL,
+	}
+	;
+	cfile.epan = epan_new(&cfile.provider, &funcs);
+	prefs_p = epan_load_settings();
+	build_column_format_array(&cfile.cinfo, prefs_p->num_cols, TRUE);
+	return 0;
+	fail:
+			clean();
 	return err;
 }
-
-gboolean read_packet(epan_dissect_t **edt_r)
-{
+gboolean read_packet(epan_dissect_t **edt_r) {
 	epan_dissect_t    *edt;
 	int                err;
 	gchar             *err_info = NULL;
 	static guint32     cum_bytes = 0;
 	static gint64      data_offset = 0;
-
 	wtap_rec rec;
 	wtap_rec_init(&rec);
 
-    /** Read the next record in the file, filling in *phdr and *buf.
+	/** Read the next record in the file, filling in *phdr and *buf.
      *
      * @wth a wtap * returned by a call that opened a file for reading.
      * @rec a pointer to a wtap_rec, filled in with information about the
@@ -140,87 +110,56 @@ gboolean read_packet(epan_dissect_t **edt_r)
      * @return TRUE on success, FALSE on failure.
      */
 	if (wtap_read(cfile.provider.wth, &rec, &cfile.buf, &err, &err_info, &data_offset)) {
-
 		cfile.count++;
-
 		frame_data fdlocal;
 		frame_data_init(&fdlocal, cfile.count, &rec, data_offset, cum_bytes);
-
 		edt = epan_dissect_new(cfile.epan, TRUE, TRUE);
 		prime_epan_dissect_with_postdissector_wanted_hfids(edt);
-
 		/**
          * Sets the frame data struct values before dissection.
          */
 		frame_data_set_before_dissect(&fdlocal, &cfile.elapsed_time, &cfile.provider.ref, cfile.provider.prev_dis);
 		cfile.provider.ref = &fdlocal;
-
-        tvbuff_t *tvb;
-        tvb = tvb_new_real_data(cfile.buf.data, data_offset, data_offset);
-
-        // core dissect process
+		tvbuff_t *tvb;
+		tvb = tvb_new_real_data(cfile.buf.data, data_offset, data_offset);
+		// core dissect process
 		epan_dissect_run_with_taps(edt, cfile.cd_t, &rec, tvb, &fdlocal, &cfile.cinfo);
-
+//		epan_dissect_run(edt, cfile.cd_t, &rec, tvb, &fdlocal, &cfile.cinfo);
+		// epan_dissect_file_run(edt, &rec, tvb, &fdlocal, &cfile.cinfo);
+		// epan_dissect_file_run_with_taps(edt, &rec, tvb, &fdlocal, &cfile.cinfo);
 		frame_data_set_after_dissect(&fdlocal, &cum_bytes);
 		cfile.provider.prev_cap = cfile.provider.prev_dis = frame_data_sequence_add(cfile.provider.frames, &fdlocal);
-
 		// free space
 		frame_data_destroy(&fdlocal);
-
 		*edt_r = edt;
 		return TRUE;
 	}
-
 	return FALSE;
 }
-
-void print_all_packet_text()
-{
+void print_all_packet_text() {
 	epan_dissect_t *edt;
 	print_stream_t *print_stream;
-
 	print_stream = print_stream_text_stdio_new(stdout);
-
-    // start reading packets
+	// start reading packets
 	while (read_packet(&edt)) {
 		proto_tree_print(print_dissections_expanded, FALSE, edt, NULL, print_stream);
-
 		epan_dissect_free(edt);
 		edt = NULL;
 	}
-
 	clean();
 }
-
-void print_first_packet_text()
-{
+void print_first_packet_text() {
 	epan_dissect_t *edt;
 	print_stream_t *print_stream;
-
 	print_stream = print_stream_text_stdio_new(stdout);
-
-    // 开始读取数据包
+	// 开始读取数据包
 	if (read_packet(&edt)) {
-	    // TODO 第二个参数无效果 需要确认
-	    proto_tree_print(print_dissections_expanded, FALSE, edt, NULL, print_stream);
-        // print hex data
-        print_hex_data(print_stream, edt);
-
-    	epan_dissect_free(edt);
-    	edt = NULL;
+		// TODO 第二个参数无效果 需要确认
+		proto_tree_print(print_dissections_expanded, FALSE, edt, NULL, print_stream);
+		// print hex data
+		print_hex_data(print_stream, edt);
+		epan_dissect_free(edt);
+		edt = NULL;
 	}
-
-    clean();
+	clean();
 }
-
-//int main()
-//{
-//    int err;
-//    char *filename = "s7comm_clean.pcap";
-//    err = init(filename);
-//    if (err) {
-//    	return 1;
-//    }
-//
-//    print_all_packet_text();
-//}
